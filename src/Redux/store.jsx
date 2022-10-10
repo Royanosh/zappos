550c997d5ec47526c4c861af96cab9a7342fda58
Gaurav Sapkal <gauravsapkal1997@gmail.com>
2022-10-10 13:20:55 +0530

    folderstructure created

---
+++ src/Redux/store.jsx
@@ -0,0 +1,25 @@
+import {
+    legacy_createStore as createStore,
+    applyMiddleware,
+    compose
+  } from "redux";
+//   import reducer from "./reducer";
+  import thunk from "redux-thunk";
+  
+  const composeEnhancers =
+    typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+          // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        })
+      : compose;
+  
+  const middlewares = applyMiddleware(thunk);
+  const enhancer = composeEnhancers(middlewares);
+  
+  export const store = createStore(reducer, enhancer);
+  
+  store.subscribe(() => {
+    store.getState();
+  });
+  
+  
