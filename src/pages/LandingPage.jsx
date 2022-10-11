5477456edebb35a35a81d2e5e3cf77db96341331
Gaurav Sapkal <gauravsapkal1997@gmail.com>
2022-10-12 02:04:45 +0530

    popular items using json server and carosule

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,6 +2,7 @@ import { Box } from "@chakra-ui/react"
 import BelowNavbarBanner from "../components/LandingPage-Components/BelowNavbarBanner";
 import Bigbanners from "../components/LandingPage-Components/Bigbanners";
 import PopularCategory from "../components/LandingPage-Components/PopularCategory";
+import PopularItems from "../components/LandingPage-Components/PopularItems";
 
 
 const LandingPage = ()=>{
@@ -12,6 +13,7 @@ const LandingPage = ()=>{
             <BelowNavbarBanner/> 
             <PopularCategory/>
             <Bigbanners/>
+            <PopularItems/>
         </Box>
     )
 }
