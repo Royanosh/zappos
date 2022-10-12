6f089519f332a0e94a4bb8fd624df19dab079d65
Gaurav Sapkal <gauravsapkal1997@gmail.com>
2022-10-12 13:59:33 +0530

    u

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { Box } from '@chakra-ui/react';
 import './App.css';
 import Navbar from './components/Navbar';
 import LandingPage from './pages/LandingPage'
-import MensClothing from './pages/MensClothing';
-import Product from './pages/Product';
-import WomensClothing from './pages/WomensClothing';
+// import MensClothing from './pages/MensClothing';
+// import Product from './pages/Product';
+// import WomensClothing from './pages/WomensClothing';
 function App() {
   return (
     <Box>
