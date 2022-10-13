6a38a8c56ab1d77223a5b3c7aa2f2c04981c42d9
Gaurav Sapkal <gauravsapkal1997@gmail.com>
2022-10-13 19:04:32 +0530

    router and arrangement done

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import Allroute from './Allroutes/Allroute';
 import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-import LandingPage from './pages/LandingPage'
-import MensClothing from './pages/MensClothing';
-import Product from './pages/Product';
-import Products from './pages/Products';
-import WomensClothing from './pages/WomensClothing';
 function App() {
   return (
     <Box>
@@ -17,14 +12,7 @@ function App() {
       <Allroute/>
 
       <Footer/> 
-  
 
-      <LandingPage/>
-
-        {/* <MensClothing/>  */}
-      {/* <WomensClothing/> */}
-      <Product/> 
-      {/* <Products/> */}
     </Box>
   );
 }
