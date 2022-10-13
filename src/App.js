6be5d9cdf806faab3422f8a94713db25cb76205a
Gaurav Sapkal <gauravsapkal1997@gmail.com>
2022-10-13 19:05:25 +0530

    app

--- src/App.js
+++ src/App.js
@@ -3,25 +3,16 @@ import Allroute from './Allroutes/Allroute';
 import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-import LandingPage from './pages/LandingPage'
-import MensClothing from './pages/MensClothing';
-import Product from './pages/Product';
-import Products from './pages/Products';
-import WomensClothing from './pages/WomensClothing';
 function App() {
   return (
     <Box>
       <Navbar/>
 
+
       <Allroute/>
 
       <Footer/> 
 
-      
-        {/* <MensClothing/>  */}
-      {/* <WomensClothing/> */}
-      {/* <Product/>  */}
-      {/* <Products/> */}
     </Box>
   );
 }
