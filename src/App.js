6c531712792c82c6a26787cd161eadc520c408a1
rajtilak-patel <rp2380264@gmail.com>
2022-10-13 20:14:25 +0530

    added product

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import { Box } from '@chakra-ui/react';
+import Allroute from './Allroutes/Allroute';
 import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-import LandingPage from './pages/LandingPage'
-import MensClothing from './pages/MensClothing';
-import Product from './pages/Product';
-import Products from './pages/Products';
-import WomensClothing from './pages/WomensClothing';
 function App() {
   return (
     <Box>
       <Navbar/>
-      <LandingPage/>
-        {/* <MensClothing/>  */}
-      {/* <WomensClothing/> */}
-      <Product/> 
-      {/* <Products/> */}
+
+
+      <Allroute/>
+
+      <Footer/> 
+
     </Box>
   );
 }
